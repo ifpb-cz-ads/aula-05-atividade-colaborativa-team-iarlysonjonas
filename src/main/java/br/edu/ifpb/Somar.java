da86adab50e7409f2d065b021dd20d11965f6f64
Jonas-Gomes <jonaszd99@gmail.com>
2020-10-19 13:25:44 -0300

    arquivos do projeto

--- src/main/java/br/edu/ifpb/Somar.java
+++ src/main/java/br/edu/ifpb/Somar.java
@@ -13,31 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 public class Somar extends HttpServlet {
 
     @Override
-        public void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         PrintWriter out = response.getWriter();
-  //      int quantidade = Integer.parseInt(request.getParameter("quantidade"));
-
-                out.print("teste");
-            }
+        int soma = 0;
+        int quantidade = Integer.parseInt(request.getParameter("quantidade"));
+        for (int i = 1; i <= quantidade; ++i) {
+            int campo = Integer.parseInt(request.getParameter("campo"+i));
+            soma = soma + campo;
         }
-
-        //for (int i = 1; i<=quantidade; i++){
-          // int valores = Integer.parseInt(request.getParameter("campo"+i));
-            //soma = soma + valores;
-            //out.println(soma);
-        //}
-        //out.println("Valor final: " + soma);
-
-        //for (int i = 0; i<valores.length; i++){
-           // soma = soma + Double.parseDouble(valores[i]);
-     //   }
-    
-      //  out.println(soma);
-    
-        
-        //request.setAttribute("soma", soma);
-        //RequestDispatcher view = request.getRequestDispatcher("form.jsp");
-        //view.forward(request, response);
- 
-    
+        out.println(soma);
+    }
+}
